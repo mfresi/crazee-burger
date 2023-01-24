4ebec5044d70805db8afb0eda40c68c4edcad76e
mfresi <mattei.fresi@gmail.com>
2023-01-24 15:50:53 +0100

    install styled component

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,5 +1,8 @@
 import LoginForm from "./LoginForm"
+import styled from "styled-components"
 
 export default function LoginPage() {
-    return <LoginForm />
+    return <LoginPageStyled>
+            <LoginForm />
+        </LoginPageStyled>
 }
