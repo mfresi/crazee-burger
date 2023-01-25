7f88e94bf1f9021fbc27d96ac2c30985cd79c053
mfresi <mattei.fresi@gmail.com>
2023-01-25 14:28:57 +0100

    ✨ ORDER > Display grid format layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -1,9 +1,14 @@
 import styled from "styled-components"
 import { theme } from "../../../theme"
+import Basket from "./Basket"
+import Menu from "./Menu"
 
 export default function Main() {
   return (
-    <MainStyled className="main" />
+    <MainStyled>
+      <Basket />
+      <Menu />
+    </MainStyled>
   )
 }
 
@@ -13,4 +18,7 @@ const MainStyled = styled.div`
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+    display: grid;
+    grid-template-columns: 25% 1fr;
 `
