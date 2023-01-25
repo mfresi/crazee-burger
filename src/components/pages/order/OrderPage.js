c7233b5fc704dcdee03f506f23bd08f428ca419a
mfresi <mattei.fresi@gmail.com>
2023-01-25 10:27:34 +0100

    ✨ F04 : OrderPage > Add container and navbar

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,15 +1,49 @@
 import { Link, useParams } from "react-router-dom"
+import styled from "styled-components"
 
 export default function OrderPage() {
 
   const {user} = useParams()
   
   return (
-    <div>
-    <div>Bienvenue {user} !</div>
-    <Link to="/">
-        <button>Déconnexion</button>
-    </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="container">
+        <div className="navbar">
+          <h1>Bienvenue {user} !</h1>
+          <Link to="/">
+              <button>Déconnexion</button>
+          </Link>
+        </div>
+        <div className="main">
+          Main
+        </div>
+      </div>
+    </OrderPageStyled>
   )
 }
+
+const OrderPageStyled = styled.div`
+  backgrouond: orange;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background: red;
+    height: 95vh;
+    width: 1400px;
+    display: flex;
+    flex-direction: column;
+
+    .navbar {
+      height: 10vh;
+      background: blue;
+    }
+
+    .main {
+      flex: 1;
+      background: green;
+    }
+  }
+`
